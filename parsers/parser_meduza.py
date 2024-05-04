04e919383ee9f8ae2b9796249ac55a41c3701785
Андрей Райцын <ranrusg@gmail.com>
2024-05-04 18:46:08 +0300

    Meduza Parser
    Added parser for meduza.io

---
+++ parsers/parser_meduza.py
@@ -0,0 +1,134 @@
+import argparse
+import math
+import os
+import time
+
+import dateparser
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from tqdm import tqdm
+
+from global_data import CHROMEDRIVER_BIN
+from parsers.parser import Parser, ParseEntity, ParseResult
+
+
+class MeduzaParser(Parser):
+
+    NEWS_ON_PAGE_NUM = 24
+    SLEEP_TIME_MORE_BUTTON = 2  # seconds
+    PAGES_NUM_BETWEEN_SLEEPS = 1
+    SLEEP_TIME_NEWS_PAGE = 5  # seconds
+
+    def __init__(self):
+        super().__init__("https://meduza.io")
+        self.selenium_service = webdriver.ChromeService(executable_path=CHROMEDRIVER_BIN)
+
+    def parse(self, news_num: int) -> ParseResult:
+        selenium_driver = webdriver.Chrome(service=self.selenium_service)
+        selenium_driver.maximize_window()
+        selenium_driver.get(self.start_url)
+
+        # Switch page to order mode
+        selenium_driver.find_element(By.CLASS_NAME,
+                                     "Switcher-module_control__60WMX").click()
+
+        # "More" button click
+        more_button_click_num = math.ceil(news_num / float(self.NEWS_ON_PAGE_NUM))
+        print(f"[MeduzaParser] Clicking to get more news ...")
+        try:
+            for i in tqdm(range(more_button_click_num)):
+                time.sleep(self.SLEEP_TIME_MORE_BUTTON)
+                self.__move_to_bottom(selenium_driver)
+                more_button = selenium_driver.find_element(By.CLASS_NAME, "Button-module_root__9OQ5b")
+                more_button.click()
+        except (ElementClickInterceptedException, NoSuchElementException):
+            print(f"[MeduzaParser][Warning] Access to the button has been blocked ...")
+        self.__move_to_bottom(selenium_driver)
+
+        # Get news titles and metadata
+        print(f"[MeduzaParser] Parse titles ...")
+        news_web_items = selenium_driver.find_elements(By.CLASS_NAME, "ChronologyItem-module-link")[:news_num]
+        news_data = []
+        for item in tqdm(news_web_items):
+            news_link = item.get_attribute('href')
+            is_story, news_title, news_subtitle = self.__get_news_title(item)
+            news_data.append([is_story, (news_title, news_subtitle), news_link])
+
+        # Get news dates and texts
+        print(f"[MeduzaParser] Parse texts ...")
+        for i, data in enumerate(tqdm(news_data)):
+            if (i % self.PAGES_NUM_BETWEEN_SLEEPS) == 0:
+                time.sleep(self.SLEEP_TIME_NEWS_PAGE)
+            news_link = data[2]
+            selenium_driver.get(news_link)
+            news_date = self.__get_news_date(selenium_driver)
+            news_text = self.__get_news_text(selenium_driver)
+            data.append(news_text)
+            data.append(news_date)
+
+        # Create ParseResult
+        parse_result = ParseResult()
+        for id, item in enumerate(tqdm(news_data)):
+            entity = ParseEntity(id=id, date=item[4], link=item[2],
+                                 title=' '.join(item[1]), text=item[3], metadata=[f'is_story: {item[0]}', ])
+            parse_result.add_entity(entity)
+
+        return parse_result
+
+    @staticmethod
+    def __move_to_bottom(driver):
+        ActionChains(driver).move_to_element(driver.find_element(By.CLASS_NAME, "Footer-module-copyright")).perform()
+
+    @staticmethod
+    def __get_news_title(driver):
+        is_story = True
+        news_block = driver.find_element(By.CLASS_NAME, "ChronologyItem-module-body")
+        news_title = news_block.find_element(By.TAG_NAME, "strong").text
+        try:
+            news_subtitle = news_block.find_element(By.TAG_NAME, "span").text
+        except NoSuchElementException:
+            news_subtitle = ""
+            is_story = False
+        return is_story, news_title, news_subtitle
+
+    @staticmethod
+    def __get_news_text(driver):
+        try:
+            news_body = driver.find_element(By.CLASS_NAME, "GeneralMaterial-module-article")
+            news_parts = news_body.find_elements(By.CLASS_NAME, "SimpleBlock-module_p__7aRnT ")
+        except NoSuchElementException:
+            return ""
+        news_parts_text = [news_part.text for news_part in news_parts]
+        return '\n'.join(news_parts_text)
+
+    @staticmethod
+    def __get_news_date(driver):
+        dates_in_text = driver.find_elements(By.TAG_NAME, "time")
+        for date_in_text in dates_in_text:
+            date_in_text = date_in_text.text
+            news_date = dateparser.parse(date_in_text)
+            if news_date:
+                return news_date
+        return None
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser(description="Parser to get fresh news articles from meduza.io")
+    args_parser.add_argument("-n", "--news-num", required=True,
+                             help="How many fresh news articles do you want to parse?")
+    args_parser.add_argument("-o", "--output", required=True,
+                             help="Output filename. Please use .csv or .xlsx extension")
+    args = args_parser.parse_args()
+
+    parser = MeduzaParser()
+    parse_data = parser.parse(int(args.news_num))
+
+    extension = os.path.splitext(args.output)[1]
+    if extension == '.csv':
+        parse_data.to_csv(args.output)
+    elif extension == '.xlsx':
+        parse_data.to_excel(args.output)
+    else:
+        raise ValueError("Incorrect output file extension! Please use .csv or .xlsx")
